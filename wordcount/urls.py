d3969a80631c56b5c218d74d1c9aac4a5f83d62d
Your Name <hammadyasir343@gmail.com>
2019-07-07 18:57:21 +0100

    second commit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.home,name="home"),
+    path('', views.homepage,name="home"),
     # path('contact/',views.contact),
     path('countss/',views.count,name="count")
 ]
